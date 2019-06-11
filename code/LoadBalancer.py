2c1134b6ba9bbc64adea6970f779f2920b7dc6dc
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-06-11 14:45:14 +0200

    777

--- code/LoadBalancer.py
+++ code/LoadBalancer.py
@@ -108,7 +108,7 @@ def handle_client(clientsocket, address):
 if __name__ == '__main__':
 	print_time('LB Started')
 	print_time('Connecting to servers')
-	server_sock = socket.socket()
+	server_sock = socket.socket(7)
 	server_sock.bind(('10.0.0.1', 80))
 	server_sock.listen()
 
